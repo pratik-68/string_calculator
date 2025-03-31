304d44906307845b755e325eb039da0f793806dd
Pratik Agarwal <pratik6868@gmail.com>
2025-03-31 22:08:57 +0530

    Step 5: Testcase for negative number exception

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -33,5 +33,9 @@ RSpec.describe StringCalculator do
     it "supports different delimiters" do
       expect(string_calculator.add("//;\n1;2")) .to eq(3)
     end
+
+    it "raises an exception for negative numbers" do
+      expect { string_calculator.add("1,-2,3,-4") }.to raise_error("negative numbers not allowed -2,-4")
+    end
   end
 end

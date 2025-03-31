0d5b0377af0d3d26fb21807bd1c0d551a4822ae6
Pratik Agarwal <pratik6868@gmail.com>
2025-03-31 22:03:42 +0530

    Step 4: Testcase for different delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -29,5 +29,9 @@ RSpec.describe StringCalculator do
     it "handles new lines between numbers" do
       expect(string_calculator.add("1\n2,3")).to eq(6)
     end
+
+    it "supports different delimiters" do
+      expect(string_calculator.add("//;\n1;2")) .to eq(3)
+    end
   end
 end

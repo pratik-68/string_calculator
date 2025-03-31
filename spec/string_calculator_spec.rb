172f04f6df3187f7bb9c0aed86be7c36602640ea
Pratik Agarwal <pratik6868@gmail.com>
2025-03-31 21:59:52 +0530

    Step 1: Testcase for sum of numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,5 +8,13 @@ RSpec.describe StringCalculator do
     it "returns 0 for an empty string" do
       expect(string_calculator.add("")).to eq(0)
     end
+
+    it "returns the number itself when only one number is provided" do
+      expect(string_calculator.add("1")).to eq(1)
+    end
+
+    it "returns sum of two numbers" do
+      expect(string_calculator.add("1,5")).to eq(6)
+    end
   end
 end
